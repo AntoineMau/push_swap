a3457a3e1a398c25d14995030716455582b10806
Antoine MAUFFRET <anmauffr@e1r13p10.42.fr>
2019-02-19 09:28:30 +0100

    push_swap

--- include/ft_checker.h
+++ include/ft_checker.h
@@ -6,13 +6,14 @@
 /*   By: anmauffr <anmauffr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/18 17:47:15 by anmauffr          #+#    #+#             */
-/*   Updated: 2019/02/18 17:50:15 by anmauffr         ###   ########.fr       */
+/*   Updated: 2019/02/19 08:48:22 by anmauffr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_CHECKER_H
 # define FT_CHECKER_H
 
+# include "../libft/includes/ftprintf.h"
 # include <unistd.h>
 # include <sys/types.h>
 # include <sys/uio.h>
